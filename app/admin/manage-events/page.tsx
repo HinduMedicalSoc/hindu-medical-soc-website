5b2a69e7d103aadab66b9f79c9ddd123d8e4a122
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2025-01-20 09:32:38 +0530

    Create new event
    Added functionality to create a new event from the admin dashboard. Could use with some UI fixed (placeholders in empty fields)

--- app/admin/manage-events/page.tsx
+++ app/admin/manage-events/page.tsx
@@ -13,6 +13,7 @@ import {
   doc,
   updateDoc,
   deleteDoc,
+  addDoc,
 } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 import { Timestamp } from "firebase/firestore";
@@ -30,6 +31,7 @@ type Event = {
 export default function ManageEvents() {
   const [events, setEvents] = useState<Event[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [createEventOpen, setCreateEventOpen] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
   const [file, setFile] = useState<File | null>(null);
   const router = useRouter();
@@ -144,6 +146,37 @@ export default function ManageEvents() {
     }
   };
 
+  //handleSubmitCreation function to create a new event, upload the image to Google Drive, and save the event to Firestore
+  const handleSubmitCreation = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!selectedEvent || !file) return;
+
+    setIsLoading(true);
+
+    try {
+      // Upload the image to Google Drive
+      const driveFileId = await uploadImageToDrive(file);
+
+      // Save the event to Firestore
+      const eventsRef = collection(db, "events");
+      const parsedDate = new Date(selectedEvent.date as string);
+      await addDoc(eventsRef, {
+        title: selectedEvent.title,
+        date: Timestamp.fromDate(parsedDate),
+        location: selectedEvent.location,
+        description: selectedEvent.description,
+        imageUrl: driveFileId, // This is the Google Drive file ID
+      });
+
+      console.log("Created event:", selectedEvent);
+    } catch (error) {
+      console.error("Error creating event:", error);
+    } finally {
+      setIsLoading(false);
+      setCreateEventOpen(false);
+    }
+  };
+
   const uploadImageToDrive = async (file: File) => {
     const formData = new FormData();
     formData.append("file", file);
@@ -169,10 +202,41 @@ export default function ManageEvents() {
     }
   };
 
+  const createNewEvent = () => {
+    setSelectedEvent({
+      id: "",
+      title: "",
+      date: "",
+      location: "",
+      description: "",
+      imageUrl: "",
+    });
+    setCreateEventOpen(true);
+  };
+
   return (
     <AuthGuard>
       <div className='relative min-h-screen p-4'>
         <h1 className='text-4xl font-bold mb-8 text-center'>Manage Events</h1>
+        <div className='flex justify-between mb-8'>
+          <div className='space-x-4'>
+            <button
+              onClick={() => router.push("/")}
+              className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'>
+              Home
+            </button>
+            <button
+              onClick={() => router.push("/admin")}
+              className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-green-600'>
+              Back to Dashboard
+            </button>
+            <button
+              onClick={() => createNewEvent()}
+              className='px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700'>
+              Add Event
+            </button>
+          </div>
+        </div>
 
         <section id='events' className='py-12 bg-gray-100'>
           <div className='container mx-auto px-4 max-w-7xl'>
@@ -315,6 +379,126 @@ export default function ManageEvents() {
                     type='submit'
                     className='px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700'>
                     Save Changes
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+
+        {/* Modal for Creating a new Event */}
+        {createEventOpen && (
+          <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
+            <div className='bg-white p-6 text-black rounded-lg w-full max-w-md mx-4'>
+              <div className='flex justify-between items-center mb-4'>
+                <h2 className='text-xl font-bold'>Create Event</h2>
+                <button
+                  onClick={() => setCreateEventOpen(false)}
+                  className='text-gray-500 hover:text-gray-700'>
+                  ✕
+                </button>
+              </div>
+
+              <form onSubmit={handleSubmitCreation} className='space-y-4'>
+                <div>
+                  <label className='block text-sm font-medium mb-1'>
+                    Title
+                  </label>
+                  <input
+                    type='text'
+                    value={selectedEvent?.title || ""}
+                    onChange={(e) =>
+                      setSelectedEvent((prev) =>
+                        prev ? { ...prev, title: e.target.value } : null
+                      )
+                    }
+                    className='w-full p-2 border rounded'
+                  />
+                </div>
+
+                <div>
+                  <label className='block text-sm font-medium mb-1'>Date</label>
+                  <input
+                    type='text'
+                    value={
+                      selectedEvent?.date instanceof Timestamp
+                        ? selectedEvent.date
+                            .toDate()
+                            .toLocaleDateString("en-US", {
+                              month: "long",
+                              day: "numeric",
+                              year: "numeric",
+                            })
+                        : selectedEvent?.date || ""
+                    }
+                    onChange={(e) =>
+                      setSelectedEvent((prev) =>
+                        prev ? { ...prev, date: e.target.value } : null
+                      )
+                    }
+                  />
+                </div>
+
+                <div>
+                  <label className='block text-sm font-medium mb-1'>
+                    Location
+                  </label>
+                  <input
+                    type='text'
+                    value={selectedEvent?.location || ""}
+                    onChange={(e) =>
+                      setSelectedEvent((prev) =>
+                        prev ? { ...prev, location: e.target.value } : null
+                      )
+                    }
+                    className='w-full p-2 border rounded'
+                  />
+                </div>
+
+                <div>
+                  <label className='block text-sm font-medium mb-1'>
+                    Description
+                  </label>
+                  <textarea
+                    value={selectedEvent?.description || ""}
+                    onChange={(e) => {
+                      setSelectedEvent((prev) =>
+                        prev ? { ...prev, description: e.target.value } : null
+                      );
+                    }}
+                    className='w-full p-2 border rounded'
+                    rows={3}
+                  />
+                </div>
+
+                <div>
+                  <label className='block text-sm font-medium mb-1'>
+                    Upload Image
+                  </label>
+                  <input
+                    type='file'
+                    accept='image/*'
+                    onChange={(e) => {
+                      const file = e.target.files?.[0];
+                      if (file) {
+                        setFile(file);
+                      }
+                    }}
+                    className='w-full p-2 border rounded'
+                  />
+                </div>
+
+                <div className='flex justify-end space-x-2 mt-6'>
+                  <button
+                    type='button'
+                    onClick={() => setCreateEventOpen(false)}
+                    className='px-4 py-2 text-gray-600 hover:text-gray-800 border rounded border-gray-300'>
+                    Cancel
+                  </button>
+                  <button
+                    type='submit'
+                    className='px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700'>
+                    Create Event
                   </button>
                 </div>
               </form>
