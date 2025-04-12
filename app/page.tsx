e2234313580a4b67f827d0ac12a6216a9c2f8158
Chinmayi Kompella <chinmayikompella@Chinmayis-MacBook-Pro.local>
2025-04-12 15:30:29 -0700

    added registration link

--- app/page.tsx
+++ app/page.tsx
@@ -158,11 +158,13 @@ export default function Home() {
                   <strong>Networking, Research, Mentorship and Seva <br />
                     A Dharmic Approach to Healthcare</strong>
                 </p>
-                <button
-                  className='mt-4 bg-white text-hmsa-blue font-bold py-2 px-6 rounded-full hover:bg-gray-200 transition duration-300'
-                  onClick={() => {}}>
-                  Register
-                </button>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSducySEnypxcisfVi-JWPdETPYBCOmJUx8uYXbYe2a-W9I4_Q/viewform?usp=header" target="_blank" rel="noopener noreferrer">
+                  <button
+                    className='mt-4 bg-white text-hmsa-blue font-bold py-2 px-6 rounded-full hover:bg-gray-200 transition duration-300'
+                    onClick={() => {}}>
+                    Register
+                  </button>
+                </a>
               </div>
             </div>
           </div>
