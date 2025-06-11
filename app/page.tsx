7d7979c4a4e97be0231052e8583134bbfe2a8360
Chinmayi Kompella <chinmayi.kompella@gmail.com>
2025-06-11 00:18:47 -0700

    Added email???

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
   return (
     <div className='min-h-screen'>
       <SmoothScroll />
-      <Header />
+      <Header/>
       <main>
       <section
         id='conf'
