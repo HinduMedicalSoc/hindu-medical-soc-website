71f0757a95b8fb7f50af9d6120b96d2b16c693a9
mangesh925 <mahirrao925@gmail.com>
2025-08-09 16:40:50 -0500

    Fix Firebase configuration and optimize EventCard component

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface EventCardProps {
@@ -25,7 +26,7 @@ const EventCard: React.FC<EventCardProps> = ({
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl hover:-translate-y-1">
       <div className="relative w-full h-auto aspect-w-4 aspect-h-3">
-        <img
+        <Image
           src={`data:image/jpeg;base64,${imageUrl}`} // Display image using the buffer data
           alt={title}
           className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
